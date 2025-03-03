5592ccb0689663d5732b6b0223dcd778a2e01f36
dcontreras1 <daniel.contreras_d@uao.edu.co>
2025-03-03 17:06:03 -0500

    Actualización EDA

--- EDA.py
+++ EDA.py
@@ -14,51 +14,52 @@ file_path = r"C:\Users\Danie\OneDrive\Escritorio\Global Economy Indicators.csv"
 df = pd.read_csv(file_path, delimiter=";")
 df.columns = df.columns.str.strip().str.replace(" ", "_")
 
-#subir datos crudos a PostgreSQL
+#Upload raw data to PostgreSQL
 df.to_sql("economy_raw", engine, if_exists="replace", index=False, method="multi", chunksize=1000)
-print("Datos cargados en PostgreSQL")
+print("Data loaded into PostgreSQL")
 
-#recuperar datos transformados desde la base de datos
+#Retrieve transformed data from the database
 df_transformed = pd.read_sql("SELECT * FROM economy_indicators_transformed", engine)
 
-#Visualizaciones
+#Visualizations
 
-#gráfico 1: Evolución PIB por país
+#Chart 1: GDP evolution by country
 plt.figure(figsize=(12,6))
 sns.lineplot(data=df_transformed, x="year", y="gdp", hue="country_name", marker="o")
-plt.title("Evolución del PIB por país")
-plt.xlabel("Año")
-plt.ylabel("PIB (GDP)")
-plt.legend(title="País", bbox_to_anchor=(1, 1))
+plt.title("GDP Evolution by Country")
+plt.xlabel("Year")
+plt.ylabel("GDP")
+plt.legend(title="Country", bbox_to_anchor=(1, 1))
 plt.grid
 plt.show()
 
-#Gráfico 2: Comparación de tasas de cambio
+#Chart 2: Exchange rate comparison
 plt.figure(figsize=(12,6))
 df_melted = df_transformed.melt(id_vars=["country_name", "year"],
                                 value_vars=["ama_exchange_rate", "imf_exchange_rate"],
-                                var_name="Tipo de cambio", value_name="Valor")
-sns.lineplot(data=df_melted, x="year", y="Valor", hue="Tipo de cambio", style="country_name", markers=True)
-plt.title("Comparación tasas de cambio")
-plt.xlabel("Año")
-plt.ylabel("Valor de cambio")
-plt.legend(title="Tipo de cambio", bbox_to_anchor=(1, 1))
+                                var_name="Exchange Rate Type", value_name="Value")
+sns.lineplot(data=df_melted, x="year", y="Value", hue="Exchange Rate Type", style="country_name", markers=True)
+plt.title("Exchange Rate Comparison")
+plt.xlabel("Year")
+plt.ylabel("Exchange Rate Value")
+plt.legend(title="Exchange Rate Type", bbox_to_anchor=(1, 1))
 plt.grid()
 plt.show()
 
-#Gráfico 3: relación entre población y GNI per capita
+#Chart 3: Relationship between population and per capita GNI
 plt.figure(figsize=(10,6))
 sns.scatterplot(data=df_transformed, x="population", y="per_capita_gni", hue="country_name", size="gni", sizes=(20, 200))
 plt.xscale("log")
-plt.title("Relación entre población y GNI per capita")
-plt.xlabel("Población")
-plt.ylabel("GNI per capita")
+plt.title("Relationship between Population and Per capita GNI")
+plt.xlabel("Population")
+plt.ylabel("per capita GNI")
 plt.show()
 
+#Chart 4: Distribution of household expenditure
 plt.figure(figsize=(10, 6))
 sns.histplot(df_transformed["household_expenditure"], bins=30, kde=True, color="blue")
-plt.title("Distribución del gasto de los hogares")
-plt.xlabel("Gasto de los hogares")
-plt.ylabel("Frecuencia")
+plt.title("Distribution of Household Expenditure")
+plt.xlabel("Household Expenditure")
+plt.ylabel("Frequency")
 plt.grid()
 plt.show()
